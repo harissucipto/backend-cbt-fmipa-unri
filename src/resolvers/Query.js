6a597f8315bcf1f7c38fb20199a069ef523e01cc
harissucipto <harisssucipto@gmail.com>
2019-02-14 18:35:22 +0700

    list dosen

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,6 @@
+// const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
+
 const Query = {
   me(parent, args, ctx, info) {
     // check if there is a current user ID
@@ -10,6 +13,19 @@ const Query = {
       },
       info,
     );
+  },
+
+  async dosens(parent, args, ctx, info) {
+    // 1. Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!');
+    }
+    console.log(ctx.request.userId);
+    // 2. Check if the user has the permissions to query all the users
+    hasPermission(ctx.request.user, ['ADMIN']);
+
+    // 3. if they do, query all the dosens!
+    return ctx.db.query.dosens(args, info);
   },
 };
 
